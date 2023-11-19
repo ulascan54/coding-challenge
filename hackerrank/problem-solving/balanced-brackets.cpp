2cdf54f0c0ee21eda2f0ebc66cac1e0364d8db69
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-11-19 17:17:42 +0300

    Balanced Brackets

---
+++ hackerrank/problem-solving/balanced-brackets.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <stack>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string isBalanced(string s)
+{
+    stack<char> store;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            store.push(s[i]);
+        }
+        else
+        {
+            if (store.empty())
+            {
+                return "NO";
+            }
+
+            char top = store.top();
+            store.pop();
+
+            if ((s[i] == ')' && top != '(') ||
+                (s[i] == ']' && top != '[') ||
+                (s[i] == '}' && top != '{'))
+            {
+                return "NO";
+            }
+        }
+    }
+
+    return store.empty() ? "YES" : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++)
+    {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
